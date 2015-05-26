fc88533b5e1737bc1057385c6b71783b1d14ab7f
daiying.zhang <zdying@live.com>
2015-05-26 19:10:40 +0800

    base

---
+++ README.md
@@ -0,0 +1,5 @@
+# lets
+
+`lets`是一个基于`Polymer`构建的可视化界面模板布局工具
+
+# other
---
+++ src/scripts/app.js
@@ -0,0 +1,35 @@
+(function(){
+    var app = document.querySelector('#app');
+
+    // 元素
+    app.menuList = [
+        {
+            title: 'List & Style',
+            icon: 'list'
+        },
+        {
+            title: 'Radio',
+            icon: 'radio-button-on'
+        },
+        {
+            title: 'Theme',
+            icon: 'extension'
+        },
+        {
+            title: 'Slider',
+            icon: 'gesture'
+        },
+        {
+            title: 'Spinner',
+            icon: 'today'
+        },
+        {
+            title: 'Tabs',
+            icon: 'label'
+        },
+        {
+            title: 'Toggle Button',
+            icon: 'label'
+        }
+    ]
+})()
